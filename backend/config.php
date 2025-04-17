88e4a81442c3dd499c6bbc8fc3c509fe01df7ede
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-04-17 11:29:11 +0530

    updated config.php

--- backend/config.php
+++ backend/config.php
@@ -2,7 +2,7 @@
 $host = 'localhost';        // Change if your MySQL host is different
 $db   = 'money_splitter';   // The database name we just created
 $db_user = 'root';             // Default username for XAMPP
-$pass = 'root';                 // Default password is empty in XAMPP
+$pass = '';                 // Default password is empty in XAMPP
 $charset = 'utf8mb4';
 
 // Set up DSN and options for PDO
